222cf2aedc8cd4418ea1d3654303a805755dba9d
satya <raj.kumar@anviam.com>
2022-07-06 16:34:58 +0530

    toast-fix

--- src/baseurl.js
+++ src/baseurl.js
@@ -1,11 +1,16 @@
 import axios from "axios";
 import { toast } from 'react-toastify';
 
-axios.create({
-    baseURL: "http://192.168.1.65:3000/api/",
-});
+// axios.create({
+//     baseURL: "http://192.168.1.65:3000/api/",
+// });
+
+axios.defaults.baseURL = "http://192.168.1.65:3000/api/";
 
 const postApi = (url, payload) => {
+
+    console.log(url);
+    
     axios.post(url, payload ).then((res) => {
         console.log(res);
         
